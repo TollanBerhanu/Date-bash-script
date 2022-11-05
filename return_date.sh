50d4f9e8c706efb753a32f6731b8e36b07bd6443
--list <tollanberhanu@gmail.com>
2022-11-05 09:50:19 +0300

    Updated to be minimal

--- return_date.sh
+++ return_date.sh
@@ -1,8 +1,1 @@
-printf -v date '%(%Y-%m-%d)T\n' -1 
-
-# put current date as yyyy-mm-dd HH:MM:SS in $date
-printf -v date '%(%Y-%m-%d %H:%M:%S)T\n' -1 
-
-# to print directly remove -v flag, as such:
 printf 'Date: %(%Y-%m-%d)T\n' -1
-# -> current date printed to terminal
